739785f59b6755035ea0e8a8a1d32466bd72bea1
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-06-26 10:49:38 +0500

    Add files via upload

---
+++ nidfosa.js
@@ -0,0 +1,11 @@
+let soliha = prompt('Ismingni kirit 🧨')
+let luch = prompt('Space ID ni kirit 🎇')
+let sheya = prompt('Space Parol ni kirit 🎑')
+
+let idk = {
+    name:soliha ,
+    id:luch,
+    parol:sheya
+}
+
+console.log(idk);
